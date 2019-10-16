f997d7b522816abeb43f222cd6e28684919649b5
Jared Stevens <jared.b.stevens@gmail.com>
2019-10-16 13:52:57 -0500

    yup

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,23 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.use(Vuetify);
 
-const opts = {};
+export default new Vuetify({
+  icons: {
+    iconfont: "md"
+  }
+});
 
-export default new Vuetify(opts);
+// export default new Vuetify({
+//   icons: {
+//     iconfont: "md"
+//   },
+//   theme: {
+//     primary: "#F32735",
+//     secondary: "#FFC629",
+//     tertiary: "#54565A"
+//   }
+// });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container>
+    <v-layout>
+      <v-btn @click="alert" text icon>close</v-btn>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  methods: {
+    alert() {
+      alert("Hi");
+    }
   }
 };
 </script>
